7907b58b60b6657dc29666f17736a0766b9ffecb
Andy Lopez <profesor.de.ingles@aol.com>
2022-07-20 21:00:19 -0500

    |- UPDATED -| new formula

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,11 +1,6 @@
-List<String> convertToF(List<double> temperaturesInC) {
-    List<String> temperaturesInF = [];
-    // print((temperaturesInC[0] * 1.8 + 32).runtimeType);
-    for (var i = 0; i < temperaturesInC.length; i++) {
-        double el = (temperaturesInC[i] * 1.8 + 32);
-        String elString = el.toStringAsFixed(2);
-        // double elDouble = double.parse(elString);
-        temperaturesInF.add(elString);
-    }
-    return temperaturesInF;
+List<double> convertToF(List<double> temperaturesInC) {
+  List<double> temperaturesInF = temperaturesInC.map((temperatureInC) {
+    return double.parse((temperatureInC * 9 / 5 + 32).toStringAsFixed(2));
+  }).toList();
+  return temperaturesInF;
 }
