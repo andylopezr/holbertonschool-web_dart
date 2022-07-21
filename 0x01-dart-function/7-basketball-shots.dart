ada67dd62aee742d8655b068974f09fd3a33666d
Andy Lopez <profesor.de.ingles@aol.com>
2022-07-20 21:03:44 -0500

    |- ADDED -| new else condition

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,13 +1,12 @@
-int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int teamAscore =
-      teamA['Free throws']! + teamA['2 pointers']! * 2 + teamA['3 pointers']! * 3;
-  int teamBscore =
-      teamB['Free throws']! + teamB['2 pointers']! * 2 + teamB['3 pointers']! * 3;
-      if(teamAscore > teamBscore){
-        return 1;
-      }
-      else if(teamAscore < teamBscore){
-        return 2;
-      }
-      return 0;
+int whoWins(Map<String, int> team1, Map<String, int> team2) {
+  int team1Score = (team1['Free throws'] ?? 0) +
+      ((team1['2 pointers'] ?? 0) * 2) +
+      ((team1['3 pointers'] ?? 0) * 2);
+  int team2Score = (team2['Free throws'] ?? 0) +
+      ((team2['2 pointers'] ?? 0) * 2) +
+      ((team2['3 pointers'] ?? 0) * 3);
+  if (team1Score == team2Score) {
+    return 0;
+  }
+  return team1Score > team2Score ? 1 : 2;
 }
