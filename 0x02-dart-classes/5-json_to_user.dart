de9972ae941f55d528165238492f043e13b9121c
Andy Lopez <profesor.de.ingles@aol.com>
2022-07-21 14:30:38 -0500

    |- UPDATED -| logic and solution

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,37 +1,32 @@
-// Update User class from 1-usertojson.dart
-// Add id property, fromJson method, and override toString method
-
 class User {
-  int id;
-  String name;
-  int age;
-  double height;
+  late int id;
+  late String name;
+  late int? age;
+  late double? height;
 
-  // Constructor with parameters - must use required so not nullable
-  User({required this.id, required this.name, required this.age, required this.height});
-
-  // Returns map representation of user
-  Map toJson() {
-    return {
-      "id": this.id,
-      "name": name,
-      "age": age,
-      "height": height
-    };
+  User({required int id, required String name, int? age, double? height}) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.height = height;
   }
 
-  // Returns string representation of user
-  // userJson is map/json representation of user - convert to string
+  Map toJson() => {
+        'id': this.id,
+        'name': this.name,
+        'age': this.age,
+        'height': this.height
+      };
+
   static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-      id: userJson["id"],
-      name: userJson["name"],
-      age: userJson["age"],
-      height: userJson["height"]
-    );
+    return new User(
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height']);
   }
 
-  // Override default toString() method
-  @override
-  String toString() => "User(id : $id ,name: $name,  age: $age, height: $height)";
+  String toString() {
+    return 'User(id : ${this.id} ,name: ${this.name},  age: ${this.age}, height: ${this.height})';
+  }
 }
