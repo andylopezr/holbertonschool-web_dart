2521aa20c9250d9784d7d31b113604c5ba3b8c5a
Andy Lopez <profesor.de.ingles@aol.com>
2022-07-20 21:02:51 -0500

    |- ADDED -| new else condition

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,13 +1,13 @@
-int whoWins(Map<String, int> team1, Map<String, int> team2) {
-  // ! is null check in case shape of map is not correct
-  // Would have preferred loop with value multiplied by key + 1 but no time currently
-  var team1Total = team1['Free throws']! +
-      team1['2 pointers']! * 2 +
-      team1['3 pointers']! * 3;
-  var team2Total = team2['Free throws']! +
-      team2['2 pointers']! * 2 +
-      team2['3 pointers']! * 3;
-  if (team1Total > team2Total) return 1;
-  if (team1Total < team2Total) return 2;
-  return 0;
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  int teamAscore =
+      teamA['Free throws']! + teamA['2 pointers']! * 2 + teamA['3 pointers']! * 3;
+  int teamBscore =
+      teamB['Free throws']! + teamB['2 pointers']! * 2 + teamB['3 pointers']! * 3;
+      if(teamAscore > teamBscore){
+        return 1;
+      }
+      else if(teamAscore < teamBscore){
+        return 2;
+      }
+      return 0;
 }
